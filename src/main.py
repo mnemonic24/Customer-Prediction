f97a5a9556c2a7bfebbbf6fe22ee0d36211b6606
tomori24 <tomori@tomori24noMacBook-Air.local>
2019-03-31 18:31:51 +0900

    rework output prediction

--- src/main.py
+++ src/main.py
@@ -20,6 +20,8 @@ MODEL_DIR_PATH = '../model/'
 ID = 'ID_code'
 TARGET = 'target'
 dtypes = setting.DTYPES
+NUM_ROUND = 1000
+N_FOLD = 10
 
 
 # return path exist (true or false)
@@ -62,8 +64,8 @@ def smote_sampling(X, y):
 def my_oversampling(data):
     print(data.shape)
     print(data[TARGET].value_counts())
-    positive_data = data[data[TARGET] == 1]
-    negative_data = data[data[TARGET] == 0]
+    positive_data = data[data[TARGET] == '1']
+    negative_data = data[data[TARGET] == '0']
     data = pd.concat([data, positive_data.sample(frac=1)], axis=0)
     data = pd.concat([data, positive_data.sample(frac=1)], axis=0)
     data = pd.concat([data, negative_data.sample(frac=1)], axis=0)
@@ -100,7 +102,7 @@ def build_xgb_model(xgtrain):
 
 
 def build_lgb_model(train, test):
-    folds = StratifiedKFold(n_splits=10, shuffle=False, random_state=0)
+    folds = StratifiedKFold(n_splits=N_FOLD, shuffle=False, random_state=0)
     preds = np.zeros(test.shape[0])
     oof = np.zeros(train.shape[0])
 
@@ -108,48 +110,37 @@ def build_lgb_model(train, test):
         print('fold: ', fold_idx)
         lgb_train = lgb.Dataset(train.iloc[train_idx][features], label=train.iloc[train_idx][TARGET])
         lgb_valid = lgb.Dataset(train.iloc[valid_idx][features], label=train.iloc[valid_idx][TARGET], reference=lgb_train)
-        # model = lgb.LGBMClassifier(boosting_type='gbdt',
-        #                            num_leaves=13,
-        #                            max_depth=-1,
-        #                            learning_rate=0.01,
-        #                            n_estimators=200,
-        #                            subsample_for_bin=2000000,
-        #                            objective='binary',
-        #                            class_weight='balanced',
-        #                            min_split_gain=0,
-        #                            min_child_samples=20,
-        #                            subsample=1,
-        #                            subsample_freq=0,
-        #                            colsample_bytree=1,
-        #                            reg_alpha=0,
-        #                            reg_lambda=0,
-        #                            random_state=0,
-        #                            n_jobs=-1,
-        #                            silent=True,
-        #                            importance_type='split',
-        #                            metric='auc')
-        # params = model.get_params()
         params = {
+            'task': 'train',
+            'objective': 'binary',
             'bagging_freq': 5,
             'bagging_fraction': 0.4,
-            'boost_from_average': 'false',
+            'boost_from_average': 'False',
             'boost': 'gbdt',
             'feature_fraction': 0.05,
             'learning_rate': 0.01,
             'max_depth': -1,
-            'metric':'auc',
+            'metric': 'auc',
             'min_data_in_leaf': 80,
             'min_sum_hessian_in_leaf': 10.0,
             'num_leaves': 13,
             'num_threads': 8,
             'tree_learner': 'serial',
-            'objective': 'binary',
-            'verbosity': 1
+            'verbosity': 1,
+            'is_unbalance': True
         }
-        clf = lgb.train(params=params, train_set=lgb_train, valid_sets=lgb_valid, num_boost_round=100, verbose_eval=1000, early_stopping_rounds=500)
+        clf = lgb.train(params=params,
+                        train_set=lgb_train,
+                        valid_sets=[lgb_train, lgb_valid],
+                        valid_names=['tarin', 'valid'],
+                        num_boost_round=NUM_ROUND,
+                        verbose_eval=100,
+                        early_stopping_rounds=50)
         oof[valid_idx] = clf.predict(train.iloc[valid_idx][features], num_iteration=clf.best_iteration)
         preds += clf.predict(test[features], num_iteration=clf.best_iteration) / folds.n_splits
     oof_score = roc_auc_score(train[TARGET], oof)
+    print(oof)
+    # acc_score, class_report, auc_score = model_score(train[TARGET], oof)
     return preds, oof_score
 
 
@@ -173,8 +164,8 @@ if __name__ == '__main__':
     with multiprocessing.Pool() as pool:
         df_train, df_test = pool.map(load_file, ["train", "test"])
 
-    # df_train = df_train.sample(2000)
-    # df_test = df_test.sample(2000)
+    # df_train = df_train.sample(5000)
+    # df_test = df_test.sample(5000)
     print(df_train.info())
     print(df_test.info())
 
@@ -188,28 +179,18 @@ if __name__ == '__main__':
     #
     # pre processing (oversampling and scaling)
     #
+
     df_train = my_oversampling(df_train)
-
-    # x_train = df_train[features]
-    # y_train = df_train[TARGET]
-
-    # x_train, y_train = smote_sampling(x_train, y_train)
+    # df_train[features], df_train[TARGET] = smote_sampling(df_train[features], df_train[TARGET])
 
     # df_train[features] = scaling(df_train[features])
     # df_test[features] = scaling(df_test[features])
-
-    # x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, test_size=0.2,
-    #                                                       random_state=0)
 
     #
     # training model and save
     #
 
-    # clf, ntree_limit = build_xgb_model(xgb.DMatrix(x_train, y_train))
-    # lgb_clf = build_lgb_model(x_train, y_train)
-
-    y_proba, total_score = build_lgb_model(df_train, df_test)
-    print(y_proba)
+    y_pred, total_score = build_lgb_model(df_train, df_test)
 
     #
     # print predict and score
@@ -232,10 +213,9 @@ if __name__ == '__main__':
     #
 
     # y_proba = lgb_clf.predict(x_test)
-    y_pred = np.where(y_proba > 0.5, 1, 0)
+    # y_pred = np.where(y_pred > 0.5, 1, 0)
+    # y_pred = np.argmax(y_pred, axis=1)
     df_test_pred = pd.Series(y_pred, index=df_test[ID], name=TARGET)
-    print(df_test_pred.head())
-    print(df_test_pred.value_counts())
     str_nowtime = datetime.now().strftime("%Y%m%d%H%M%S")
     df_test_pred.to_csv(SUBMIT_DIR_PATH + f'submit_{str_nowtime}_{round(total_score * 100, 2)}.csv',
                         header=True)
